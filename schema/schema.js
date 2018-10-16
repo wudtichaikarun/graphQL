ca20cdfeefcca273159e05ff467983947264921c
romantic <cesc_fabregas1414@hotmail.com>
2018-10-16 17:02:51 +0700

    feat(schema) : CRUD

--- schema/schema.js
+++ schema/schema.js
@@ -97,9 +97,38 @@ const mutation = new GraphQLObjectType({
           })
           .then(resp => resp.data);
       }
+    },
+    eidtUser: {
+      type: UserType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLString) },
+        firstName: { type: GraphQLString },
+        age: { type: GraphQLInt },
+        companyId: { type: GraphQLString }
+      },
+      resolve(parentValue, args) {
+        return axios
+          .patch(
+            `http://localhost:3000/users/${args.id}`,
+            args
+          )
+          .then(resp => resp.data);
+      }
+    },
+    deleteUser: {
+      type: UserType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLString) }
+      },
+      resolve(parentValue, { id }) {
+        return axios
+          .delete(`http://localhost:3000/users/${id}`)
+          .then(resp => resp.data);
+      }
     }
   }
 });
+
 module.exports = new GraphQLSchema({
   query: RootQuery,
   mutation
