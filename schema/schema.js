e280223935b037dad209bd617c9f1c0fda8848e1
romantic <cesc_fabregas1414@hotmail.com>
2018-10-16 08:44:57 +0700

    feat(schema.js) : nested queries

--- schema/schema.js
+++ schema/schema.js
@@ -7,12 +7,33 @@ const {
   GraphQLSchema
 } = graphql;
 
+const CompanyType = new GraphQLObjectType({
+  name: 'Company',
+  fields: {
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString }
+  }
+});
+
 const UserType = new GraphQLObjectType({
   name: 'User',
   fields: {
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
-    age: { type: GraphQLInt }
+    age: { type: GraphQLInt },
+    company: {
+      type: CompanyType,
+      resolve(parentValue, args) {
+        return axios
+          .get(
+            `http://localhost:3000/companies/${
+              parentValue.CompanyId
+            }`
+          )
+          .then(resp => resp.data);
+      }
+    }
   }
 });
 
